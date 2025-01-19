fc33456e17a83a386f232be6a2d8e966c45e6798
Abhiru Karki <abhirukarki311@gmail.com>
2025-01-19 15:49:37 +0000

    Add Display Functionality

--- script.js
+++ script.js
@@ -1,4 +1,7 @@
 const myBooks = [];
+const btn = document.querySelector(".button-area");
+
+const cardArea = document.querySelector(".card-area");
 
 function Book(title, author, pages, readStatus) {
   this.title = title;
@@ -12,14 +15,25 @@ function addBookToLibrary(title, author, pages, readStatus) {
   myBooks.push(newBook);
 }
 
+function clearCardArea() {
+  cardArea.innerHTML = " ";
+}
+
 function displayBooks() {
   for (let i = 0; i < myBooks.length; i++) {
-    console.log(myBooks[i]);
+    const card = document.createElement("section");
+    card.classList.add("card");
+    card.innerHTML = `<p class="card__title">${myBooks[i].title}</p>
+            <p class="card__author"><b>Author:</b> ${myBooks[i].author}</p>
+            <p class="card__pages"><b>Pages:</b> ${myBooks[i].pages}</p>
+            <p class="card__readStatus"><b>Read:</b> ${myBooks[i].readStatus}</p>`;
+    cardArea.appendChild(card);
   }
 }
 
-addBookToLibrary("Harry Potter", "J.K. Rowling", 226, "Completed");
-addBookToLibrary("Harry", "Rowling", 276, "Not Completed");
-addBookToLibrary("Potter", "J.K.", 206, "Want to Read");
+clearCardArea();
+addBookToLibrary("Book Title 1", "Full Name", "500", "Not Completed");
+addBookToLibrary("Book Title 2", "Full Name", "300", "Completed");
+addBookToLibrary("Book Title 3", "Full Name", "200", "Completed");
 
 displayBooks();
